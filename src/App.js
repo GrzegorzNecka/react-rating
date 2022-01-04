bc5cf8a17c271795e4ad7008dfdc020e29ef8ac1
GrzegorzNecka <strzypior@gmail.com>
2022-01-04 09:09:18 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
+const UglyNumber = () => {
+  const [state, setState] = useState(0);
+
+  useEffect(() => {
+    setState(state + 1);
+    setState(state + 1);
+    setState(state + 1);
+  }, []);
+
+  return <p>Ugly Number: {state}</p>;
+};
+
 export default function App() {
-  const [rating, setRating] = useState(0);
-  const stars = [1, 2, 3, 4, 5];
-
-  const handleReset = (e) => {
-    if (e.target.tagName != 'DIV') {
-      return;
-    }
-    setRating(0);
-  };
-
   return (
     <div onClick={handleReset} className="rating">
       {stars.map((star) => {
